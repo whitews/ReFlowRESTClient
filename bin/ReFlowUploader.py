977bfd06fd19df36283a1cc61ff53a641bc55b26
Scott White <whitews@gmail.com>
2013-04-22 14:13:13 -0400

    added logo image

--- bin/ReFlowUploader.py
+++ bin/ReFlowUploader.py
@@ -1,4 +1,7 @@
 import Tkinter as tk
+from PIL import Image, ImageTk
+
+LOGO_PATH = '../imgs/reflow_text.png'
 
 
 class Application(tk.Frame):
@@ -6,17 +9,23 @@ class Application(tk.Frame):
     def __init__(self, master):
         # can't call super on old-style class, call parent init directly
         tk.Frame.__init__(self, master)
-
+        self.master.title('ReFlow Uploader')
+        self.master.minsize(width=800, height=640)
+        self.master.config(bg="#f5f5f5")
         self.pack()
         self.createWidgets()
 
     def createWidgets(self):
+        self.logoImage = ImageTk.PhotoImage(Image.open(LOGO_PATH))
+        self.logoPanel = tk.Label(self, image=self.logoImage)
+        self.logoPanel.config(bg="#f5f5f5")
+        self.logoPanel.pack(side='top')
+
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.quitButton.config(bg="#f5f5f5")
         self.quitButton.pack()
+
 
 root = tk.Tk()
 app = Application(root)
-app.master.title('ReFlow Uploader')
-app.master.minsize(width=200, height=200)
-
 app.mainloop()
