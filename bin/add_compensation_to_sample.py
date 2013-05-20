69917a5de7ea814b6234ae69c7458f4e5e98a310
Scott White <whitews@gmail.com>
2013-05-20 14:12:02 -0400

    - update panel/comp association scripts to loop so user doesn't have to enter creds every time

--- bin/add_compensation_to_sample.py
+++ bin/add_compensation_to_sample.py
@@ -18,100 +18,116 @@ else:
     sys.exit()
 
 
-# Projects
-project_list = get_projects(host, token)
+def start():
+    # Projects
+    project_list = get_projects(host, token)
 
-for i, result in enumerate(project_list['data']):
-    print i, ':', result['project_name']
+    for i, result in enumerate(project_list['data']):
+        print i, ':', result['project_name']
 
-project_choice = raw_input('Choose Project:')
-project = project_list['data'][int(project_choice)]
+    project_choice = raw_input('Choose Project:')
+    project = project_list['data'][int(project_choice)]
 
+    # Subjects
+    subject_list = get_subjects(host, token, project_pk=project['id'])
 
-# Sites
-site_list = get_sites(host, token, project_pk=project['id'])
+    for i, result in enumerate(subject_list['data']):
+        print i, ':', result['subject_id']
 
-if not site_list:
-    sys.exit('There are no sites')
+    subject_choice = raw_input('Choose Subject (leave blank for all subjects): ')
+    subject = None
+    if subject_choice:
+        subject = subject_list['data'][int(subject_choice)]
 
-for i, result in enumerate(site_list['data']):
-    print i, ':', result['site_name']
+    # Sites
+    site_list = get_sites(host, token, project_pk=project['id'])
 
-site_choice = raw_input('Choose Site (required): ')
-site = site_list['data'][int(site_choice)]
+    if not site_list:
+        sys.exit('There are no sites')
 
+    for i, result in enumerate(site_list['data']):
+        print i, ':', result['site_name']
 
-# Samples
-sample_args = [host, token]
-sample_kwargs = {'site_pk': site['id']}
-sample_list = get_samples(*sample_args, **sample_kwargs)
-
-if not sample_list:
-    sys.exit('There are no samples')
-
-for i, result in enumerate(sample_list['data']):
-    print i, ':', result['original_filename']
-
-sample_choice = raw_input('Choose Sample (leave blank for all samples): ')
-sample = None
-if sample_choice:
-    sample = sample_list['data'][int(sample_choice)]
+    site_choice = raw_input('Choose Site (required): ')
+    site = site_list['data'][int(site_choice)]
 
 
-# Compensation
-compensation_list = get_compensations(host, token, site_pk=site['id'], project_pk=project['id'])
+    # Samples
+    sample_args = [host, token]
+    sample_kwargs = {'site_pk': site['id']}
+    if subject:
+        sample_kwargs['subject_pk'] = subject['id']
+    sample_list = get_samples(*sample_args, **sample_kwargs)
 
-if not compensation_list:
-    sys.exit('There are no compensations')
-for i, result in enumerate(compensation_list['data']):
-    print i, ':', result['original_filename']
+    if not sample_list:
+        sys.exit('There are no samples')
 
-compensation_choice = raw_input('Choose Compensation (required): ')
-compensation = compensation_list['data'][int(compensation_choice)]
+    for i, result in enumerate(sample_list['data']):
+        print i, ':', result['original_filename']
 
-# Now have user verify information
-print '=' * 40
-print 'You chose to add this compensation to these samples:'
+    sample_choice = raw_input('Choose Sample (leave blank for all samples): ')
+    sample = None
+    if sample_choice:
+        sample = sample_list['data'][int(sample_choice)]
 
-print '\Compensation: %s' % compensation['original_filename']
 
-print 'Samples:'
-if sample:
-    print '\t%s' % sample['original_filename']
-else:
-    for s in sample_list['data']:
-        print '\t%s' % s['original_filename']
-print '=' * 40
+    # Compensation
+    compensation_list = get_compensations(host, token, site_pk=site['id'], project_pk=project['id'])
 
-apply_choice = None
-while apply_choice not in ['continue', 'exit']:
-    apply_choice = raw_input("Type 'continue' to upload, 'exit' abort: ")
-    if apply_choice == 'exit':
-        sys.exit()
+    if not compensation_list:
+        sys.exit('There are no compensations')
+    for i, result in enumerate(compensation_list['data']):
+        print i, ':', result['original_filename']
 
-print 'continue'
+    compensation_choice = raw_input('Choose Compensation (required): ')
+    compensation = compensation_list['data'][int(compensation_choice)]
 
-if sample:
-    response_dict = add_compensation_to_sample(
-        host,
-        token,
-        sample_pk=str(sample['id']),
-        compensation_pk=str(compensation['id'])
-    )
+    # Now have user verify information
+    print '=' * 40
+    print 'You chose to add this compensation to these samples:'
 
-    print "Response: ", response_dict['status'], response_dict['reason']
-    print 'Data: '
-    print json.dumps(response_dict['data'], indent=4)
+    print '\Compensation: %s' % compensation['original_filename']
 
-else:
-    for sample in sample_list['data']:
+    print 'Samples:'
+    if sample:
+        print '\t%s' % sample['original_filename']
+    else:
+        for s in sample_list['data']:
+            print '\t%s' % s['original_filename']
+    print '=' * 40
+
+    apply_choice = None
+    while apply_choice not in ['continue', 'exit']:
+        apply_choice = raw_input("Type 'continue' to upload, 'exit' abort: ")
+        if apply_choice == 'exit':
+            sys.exit()
+
+    print 'continue'
+
+    if sample:
         response_dict = add_compensation_to_sample(
             host,
             token,
             sample_pk=str(sample['id']),
-            compensation_pk=str(compensation['id']),
+            compensation_pk=str(compensation['id'])
         )
 
         print "Response: ", response_dict['status'], response_dict['reason']
         print 'Data: '
         print json.dumps(response_dict['data'], indent=4)
+
+    else:
+        for sample in sample_list['data']:
+            response_dict = add_compensation_to_sample(
+                host,
+                token,
+                sample_pk=str(sample['id']),
+                compensation_pk=str(compensation['id']),
+            )
+
+            print "Response: ", response_dict['status'], response_dict['reason']
+            print 'Data: '
+            print json.dumps(response_dict['data'], indent=4)
+
+while True:
+    start()
--- bin/apply_panel_to_samples.py
+++ bin/apply_panel_to_samples.py
@@ -18,128 +18,132 @@ else:
     sys.exit()
 
 
-# Projects
-project_list = get_projects(host, token)
+def start():
+    # Projects
+    project_list = get_projects(host, token)
 
-for i, result in enumerate(project_list['data']):
-    print i, ':', result['project_name']
+    for i, result in enumerate(project_list['data']):
+        print i, ':', result['project_name']
 
-project_choice = raw_input('Choose Project:')
-project = project_list['data'][int(project_choice)]
-
-
-# Visits
-visit_type_list = get_visit_types(host, token, project_pk=project['id'])
-
-for i, result in enumerate(visit_type_list['data']):
-    print i, ':', result['visit_type_name']
-
-visit_type_choice = raw_input('Choose Visit Type (leave blank for all types): ')
-visit_type = None
-if visit_type_choice:
-    visit_type = visit_type_list['data'][int(visit_type_choice)]
+    project_choice = raw_input('Choose Project:')
+    project = project_list['data'][int(project_choice)]
 
 
-# Subjects
-subject_list = get_subjects(host, token, project_pk=project['id'])
+    # Visits
+    visit_type_list = get_visit_types(host, token, project_pk=project['id'])
 
-for i, result in enumerate(subject_list['data']):
-    print i, ':', result['subject_id']
+    for i, result in enumerate(visit_type_list['data']):
+        print i, ':', result['visit_type_name']
 
-subject_choice = raw_input('Choose Subject (leave blank for all subjects): ')
-subject = None
-if subject_choice:
-    subject = subject_list['data'][int(subject_choice)]
-
-
-# Sites
-site_list = get_sites(host, token, project_pk=project['id'])
-
-if not site_list:
-    sys.exit('There are no sites')
-
-for i, result in enumerate(site_list['data']):
-    print i, ':', result['site_name']
-
-site_choice = raw_input('Choose Site (required): ')
-site = site_list['data'][int(site_choice)]
+    visit_type_choice = raw_input('Choose Visit Type (leave blank for all types): ')
+    visit_type = None
+    if visit_type_choice:
+        visit_type = visit_type_list['data'][int(visit_type_choice)]
 
 
-# Samples
-sample_args = [host, token]
-sample_kwargs = {'site_pk': site['id']}
-if subject:
-    sample_kwargs['subject_pk'] = subject['id']
-if visit_type:
-    sample_kwargs['visit_pk'] = visit_type['id']
-sample_list = get_samples(*sample_args, **sample_kwargs)
+    # Subjects
+    subject_list = get_subjects(host, token, project_pk=project['id'])
 
-if not sample_list:
-    sys.exit('There are no samples')
+    for i, result in enumerate(subject_list['data']):
+        print i, ':', result['subject_id']
 
-for i, result in enumerate(sample_list['data']):
-    print i, ':', result['original_filename']
-
-sample_choice = raw_input('Choose Sample (leave blank for all samples): ')
-sample = None
-if sample_choice:
-    sample = sample_list['data'][int(sample_choice)]
+    subject_choice = raw_input('Choose Subject (leave blank for all subjects): ')
+    subject = None
+    if subject_choice:
+        subject = subject_list['data'][int(subject_choice)]
 
 
-# Panel
-panel_list = get_panels(host, token, site_pk=site['id'], project_pk=project['id'])
+    # Sites
+    site_list = get_sites(host, token, project_pk=project['id'])
 
-if not panel_list:
-    sys.exit('There are no panels')
-for i, result in enumerate(panel_list['data']):
-    print i, ':', result['panel_name']
+    if not site_list:
+        sys.exit('There are no sites')
 
-panel_choice = raw_input('Choose Panel (required): ')
-panel = panel_list['data'][int(panel_choice)]
+    for i, result in enumerate(site_list['data']):
+        print i, ':', result['site_name']
 
-# Now have user verify information
-print '=' * 40
-print 'You chose to apply this panel to these samples:'
+    site_choice = raw_input('Choose Site (required): ')
+    site = site_list['data'][int(site_choice)]
 
-print '\tPanel: %s' % panel['panel_name']
 
-print 'Samples:'
-if sample:
-    print '\t%s' % sample['original_filename']
-else:
-    for s in sample_list['data']:
-        print '\t%s' % s['original_filename']
-print '=' * 40
+    # Samples
+    sample_args = [host, token]
+    sample_kwargs = {'site_pk': site['id']}
+    if subject:
+        sample_kwargs['subject_pk'] = subject['id']
+    if visit_type:
+        sample_kwargs['visit_pk'] = visit_type['id']
+    sample_list = get_samples(*sample_args, **sample_kwargs)
 
-apply_choice = None
-while apply_choice not in ['continue', 'exit']:
-    apply_choice = raw_input("Type 'continue' to upload, 'exit' abort: ")
-    if apply_choice == 'exit':
-        sys.exit()
+    if not sample_list:
+        sys.exit('There are no samples')
 
-print 'continue'
+    for i, result in enumerate(sample_list['data']):
+        print i, ':', result['original_filename']
 
-if sample:
-    response_dict = patch_sample_with_panel(
-        host,
-        token,
-        sample_pk=str(sample['id']),
-        panel_pk=str(panel['id'])
-    )
+    sample_choice = raw_input('Choose Sample (leave blank for all samples): ')
+    sample = None
+    if sample_choice:
+        sample = sample_list['data'][int(sample_choice)]
 
-    print "Response: ", response_dict['status'], response_dict['reason']
-    print 'Data: '
-    print json.dumps(response_dict['data'], indent=4)
 
-else:
-    for sample in sample_list['data']:
+    # Panel
+    panel_list = get_panels(host, token, site_pk=site['id'], project_pk=project['id'])
+
+    if not panel_list:
+        sys.exit('There are no panels')
+    for i, result in enumerate(panel_list['data']):
+        print i, ':', result['panel_name']
+
+    panel_choice = raw_input('Choose Panel (required): ')
+    panel = panel_list['data'][int(panel_choice)]
+
+    # Now have user verify information
+    print '=' * 40
+    print 'You chose to apply this panel to these samples:'
+
+    print '\tPanel: %s' % panel['panel_name']
+
+    print 'Samples:'
+    if sample:
+        print '\t%s' % sample['original_filename']
+    else:
+        for s in sample_list['data']:
+            print '\t%s' % s['original_filename']
+    print '=' * 40
+
+    apply_choice = None
+    while apply_choice not in ['continue', 'exit']:
+        apply_choice = raw_input("Type 'continue' to upload, 'exit' abort: ")
+        if apply_choice == 'exit':
+            sys.exit()
+
+    print 'continue'
+
+    if sample:
         response_dict = patch_sample_with_panel(
             host,
             token,
             sample_pk=str(sample['id']),
-            panel_pk=str(panel['id']),
+            panel_pk=str(panel['id'])
         )
 
         print "Response: ", response_dict['status'], response_dict['reason']
         print 'Data: '
         print json.dumps(response_dict['data'], indent=4)
+
+    else:
+        for sample in sample_list['data']:
+            response_dict = patch_sample_with_panel(
+                host,
+                token,
+                sample_pk=str(sample['id']),
+                panel_pk=str(panel['id']),
+            )
+
+            print "Response: ", response_dict['status'], response_dict['reason']
+            print 'Data: '
+            print json.dumps(response_dict['data'], indent=4)
+
+while True:
+    start()
