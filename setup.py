e4ddae772101ce25015d8d4423404925c0a594e4
Scott White <whitews@gmail.com>
2013-12-18 14:58:20 -0500

    added processing module to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     author='Scott White',
     author_email='scott.white@duke.edu',
-    packages=['reflowrestclient'],
+    packages=['reflowrestclient', 'reflowrestclient.processing'],
     url='https://github.com/whitews/ReFlowRESTClient',
     license='LICENSE.txt',
     description='REST client for interacting with a ReFlow repository.',
