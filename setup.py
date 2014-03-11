0d1d79a2347da9c7f005cd4adfe8e6ee22ed9145
Scott White <whitews@gmail.com>
2014-03-11 12:37:26 -0400

    bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='reflowrestclient',
-    version='0.0.2',
+    version='0.0.3',
     author='Scott White',
     author_email='scott.white@duke.edu',
     packages=['reflowrestclient', 'reflowrestclient.processing'],
