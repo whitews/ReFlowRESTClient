762968d776c21137aca9ed09b84b57292911654e
Scott White <whitews@gmail.com>
2014-07-29 15:08:58 -0400

    version 0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='reflowrestclient',
-    version='0.0.7',
+    version='0.0.8',
     author='Scott White',
     author_email='scott.white@duke.edu',
     packages=['reflowrestclient'],
