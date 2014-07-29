23b9bc1aaea6772247df644de1ae1c6b79bccf42
Scott White <whitews@gmail.com>
2014-07-29 11:40:29 -0400

    update example login script with method

--- examples/example_login.py
+++ examples/example_login.py
@@ -1,12 +1,13 @@
 import getpass
-from reflowrestclient.utils import get_token
+from reflowrestclient.utils import get_token, METHOD
 
 host = "localhost:8000"
 
 username = raw_input('Username: ')
 password = getpass.getpass('Password: ')
+method = raw_input('Method (https or http): ')
 
-token = get_token(host, username, password)
+token = get_token(host, username, password, method=METHOD[method])
 
 if token:
     print "Got token!!!"
